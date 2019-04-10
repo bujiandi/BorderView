d00d0e5282e2dc8f6e753106faf6ceb3b4f2b7f3
bujiandi <lzl@appfenfen.com>
2019-04-10 14:10:57 +0800

    v0.1.0

---
+++ BorderView/Supporting Files/BorderView.h
@@ -0,0 +1,19 @@
+//
+//  BorderView.h
+//  BorderView
+//
+//  Created by 李招利 on 2018/8/20.
+//  Copyright © 2018年 李招利. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BorderView.
+FOUNDATION_EXPORT double BorderViewVersionNumber;
+
+//! Project version string for BorderView.
+FOUNDATION_EXPORT const unsigned char BorderViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BorderView/PublicHeader.h>
+
+
